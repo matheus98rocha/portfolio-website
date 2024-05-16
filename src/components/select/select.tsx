5de178523568b5b38c78df5c54373af1a110578a
matheus98rocha <matheus98rocha@gmail.com>
2024-05-15 21:48:23 -0300

    feat: ajuste select

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { useLocale } from 'next-intl'
 import { useRouter } from 'next/navigation'
 
+import * as Io from 'react-icons/io'
+
 function Select() {
   const [selectedLocale, setSelectedLocale] = useState(useLocale())
   const [isOpenSelect, setIsOpenSelect] = useState<boolean>(false)
@@ -26,13 +28,15 @@ function Select() {
 
   return (
     <div className='relative'>
-      <div className='bg-transparent py-2 text-stone-100'>
-        <p
-          className='cursor-pointer'
-          onClick={() => setIsOpenSelect(!isOpenSelect)}
-        >
-          {selectedLocale.toUpperCase()}
-        </p>
+      <div
+        className='flex cursor-pointer items-center justify-center bg-transparent py-2 text-stone-100'
+        onClick={() => setIsOpenSelect(!isOpenSelect)}
+      >
+        <p>{selectedLocale.toUpperCase()}</p>
+        <Io.IoMdArrowDropup
+          color='white'
+          className={`text-white ${!isOpenSelect ? 'rotate-180' : ''} transition delay-75 duration-75 ease-in-out`}
+        />
         <div className='absolute top-10 cursor-pointer'>
           {isOpenSelect &&
             langs
